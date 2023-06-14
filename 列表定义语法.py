23c2cd224493aa7d2423f5c2ed0f38690ef97b32
iuei <115651522+iuei@users.noreply.github.com>
2023-06-14 21:45:39 +0800

    Add files via upload

---
+++ 列表定义语法.py
@@ -0,0 +1,13 @@
+# 定义一个list列表
+a_list = ["name", "age", "id"]
+# 输出列表
+print(a_list)
+# 使用type查看列表类型
+print(type(a_list))
+
+# 定义一个嵌套列表
+b_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# 输出嵌套列表
+print(b_list)
+# 使用type查看列表类型
+print(type(b_list))
