8bbcc6742459c7f884012a1ff72dbfa689e428d6
iuei <115651522+iuei@users.noreply.github.com>
2023-06-14 23:37:09 +0800

    Add files via upload

---
+++ 列表索引.py
@@ -0,0 +1,16 @@
+# 定义一个列表
+a_list = ["name", "age", "id"]
+# 定义一个嵌套列表
+b_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# 索引列表元素 正向索引：从0开始递增
+print(a_list[0])
+print(a_list[1])
+print(a_list[2])
+print(b_list[1][1])
+
+# 反向索引列表元素 反向索引：从-1开始递减
+print(a_list[-1])
+print(a_list[-2])
+print(a_list[-3])
+print(b_list[-1][-1])
