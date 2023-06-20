1b42e26cdc3c3bac4591c1d42b5ac2539b84af47
iuei <115651522+iuei@users.noreply.github.com>
2023-06-20 21:32:04 +0800

    Add files via upload

--- 序列切片例题.py
+++ 序列切片例题.py
@@ -1,4 +1,9 @@
 # 得到"黑马程序员"
 str1 = "万过薪月，员序程马黑来，nohtyP学"
-result = str1[::-1].split("，")[1][::-1]
-print(result)
+result = str1[::-1][9:14:1]
+print(f'方法1：{result}')
+
+# 方式2
+str1 = "万过薪月，员序程马黑来，nohtyP学"
+my_list = str1[5:10:1][::-1]
+print(f'方法2：{my_list}')
