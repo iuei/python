e916d94c02b404afcf386c262b340ffa179cb238
iuei <115651522+iuei@users.noreply.github.com>
2023-06-24 22:03:45 +0800

    Add files via upload

---
+++ 因式分解.py
@@ -0,0 +1,16 @@
+x = input('请输入一个小于1000的整数：')
+x = int(x)
+t = x
+i = 2
+result = []
+
+while True:
+    if t == 1:
+        break
+    if t % i == 0:
+        result.append(i)
+        t = t // i
+    else:
+        i += 1
+
+print(x, "=", "*".join(map(str, result)))
